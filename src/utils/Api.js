2fb82b483fc5e614d47690bd8140dd59ede892fe
DevDeb <deborah.santucci@cleafy.com>
2022-02-01 10:03:30 +0100

    [#7] remove unused method

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,12 +3,6 @@ import axios from 'axios'
 const restCountries = axios.create({ baseURL: 'https://restcountries.com/v3.1' })
 
 export default class Api {
-  static getCountryByName = (name) =>
-    restCountries
-      .get(`/name/${name}`)
-      .then((response) => response.data)
-      .catch((error) => error)
-
   static getAllCountries = () =>
     restCountries
       .get('/all')
